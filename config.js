83b5db23c18cc6809b0e93ea12410158936a28e5
Zyne <fiqitzy11277@gmail.com>
2026-04-22 00:05:08 +0700

    ./

---
+++ config.js
@@ -0,0 +1,11 @@
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6283134151058"] 
+global.namabot = 'Drakvers by kiaa'
+//======================
+global.mess = { 
+owner: 'Minta ke kiaa dulu deck! ',
+premium: 'Lu bukan kiaa tolol',
+succes: 'Success by kiaa'
+}
+//======================
